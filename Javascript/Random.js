9673f82fa7d9485fff4db9ad1d404e189ea92dbb
adkevin3307 <adkevin3307@gmail.com>
2018-06-09 14:41:37 +0800

    Kevin
    delete ReadFileTest and Introduction, move javascript to another file

---
+++ Javascript/Random.js
@@ -0,0 +1,23 @@
+$(function() {
+    $('#random').attr('disabled', true);
+    $('#enter').click(function() {
+        $('#list').html('');
+        $('#result').html('');
+        if (isNaN($('#amount').val())) window.alert('Please input number');
+        else {
+            var s = '';
+            for (var i = 0; i < parseInt($('#amount').val()); i++) {
+                s += '<div>';
+                s += '第' + (i + 1) + '人姓名: ';
+                s += '<input id = "num' + i + '" type = "text" value = "Person' + (i + 1) + '">';
+                s += '</div>';
+            }
+            $('#list').html(s);
+            $('#random').attr('disabled', false);
+        }
+    });
+    $('#random').click(function() {
+        if (isNaN($('#amount').val())) window.alert('Please input number');
+        else $('#result').html($('#num' + Math.floor(Math.random() * $('#amount').val())).val());
+    });
+});
