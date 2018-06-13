12d2be6b79a78706fffbcd3affe8371fb1a01426
adkevin3307 <adkevin3307@gmail.com>
2018-06-13 13:57:09 +0800

    Kevin
    change json path

--- Javascript/Menu.js
+++ Javascript/Menu.js
@@ -1,24 +1,5 @@
 var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
 
-var jsonPath = {
-    "海大燒臘": "vpfcy",
-    "金園": "ooif6",
-    "818": "1e809u",
-    "八方雲集": "x0epu",
-    "豚將": "xlubm",
-    "三媽": "s8xvm",
-    "佬地方牛排": "1apdmq",
-    "佳汶麵食館": "nhh1e",
-    "牛餃鏖鋒": "o51sy",
-    "陳家麵店": "cttb6",
-    "晶彩": "17scrm",
-    "寶妹": "18dsde",
-    "阿水飯麵館": "13mbj6",
-    "家薌牛肉麵": "1a636q",
-    "涵館": "14t6qq",
-    "港式燒臘便當": "uxe3m"
-}
-
 $(function() {
     $('nav').mouseenter(function() {
         $('#total_select').css('height', '280px').hover(
@@ -71,7 +52,7 @@ function generateMenu(e) {
     $('.header').html(e.target.innerHTML);
     $('#menu').show();
     $('#introduce').hide();
-    $.getJSON('https://api.myjson.com/bins/' + jsonPath[e.target.id], function(data) {
+    $.getJSON('./Json' + e.target.id + '.json', function(data) {
         $("#menu").html("");
         var content = "<tr><th>菜名</th><th>價錢</th><th>菜名</th><th>價錢</th></tr>";
         for (var index = 0; index < data.length - 1; index += 2) {
